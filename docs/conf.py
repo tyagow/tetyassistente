f8dc912a51fc3b5c6638c7427683ff45e6b70a57
Tiago Almeida <tyagow@hotmail.com.br>
2017-02-07 13:44:44 -0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import os
+
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
@@ -158,3 +160,12 @@ texinfo_documents = [
 
 
 
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
