cb2d80fd887c6a0f7d584c8def05154071fd06fc
Tiago Almeida <tyagow@hotmail.com.br>
2017-02-13 13:38:34 -0200

    wsgi.py refactor DEBUG

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,18 +1,12 @@
-"""
-WSGI config for src project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
-from dj_static import Cling
+from dj_static import Cling, MediaCling
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
-application = Cling(get_wsgi_application())
-
+if settings.DEBUG:
+    application = Cling(MediaCling(get_wsgi_application()))
+else:
+    application = get_wsgi_application()
