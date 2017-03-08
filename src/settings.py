039568c04466566159cb319fe26a11cb4d875e14
Tiago Almeida <tyagow@hotmail.com.br>
2017-03-07 21:57:54 -0300

    settings without celery

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from celery.schedules import crontab
+# from celery.schedules import crontab
 from dj_database_url import parse as dburl
 from decouple import config, Csv
 
