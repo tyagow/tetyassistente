6d76747820e858084edda37a21fc9326808562f8
Tiago Almeida <tyagow@hotmail.com.br>
2017-04-17 23:08:44 -0300

    Report Form refactor, static buttom to choose type report

--- src/reports/forms.py
+++ src/reports/forms.py
@@ -1,5 +1,6 @@
 import datetime
 from django import forms
+from django.forms import HiddenInput
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
@@ -10,11 +11,11 @@ TIMESTAMP_FORMAT = "%d/%m/%Y %H:%M"
 
 class ReportForm(forms.ModelForm):
     timestamp = forms.DateTimeField(label=_('Horário'), input_formats=[TIMESTAMP_FORMAT], initial=(timezone.now()-datetime.timedelta(hours=3)).strftime(TIMESTAMP_FORMAT))
-    type = forms.ChoiceField(label=_('Tipo de report'), choices=REPORT_TYPES, initial=1, widget=forms.Select(attrs={'class':'custom-select'}),)
+    type = forms.ChoiceField(label=_('Tipo de report'), choices=REPORT_TYPES, initial=1, widget=forms.HiddenInput())
 
     class Meta:
         model = Report
         fields = ['type', 'content', 'timestamp']
-        labels = {
-            'type': _('Tipos de report'),
-        }
+        # labels = {
+        #     'type': _('Tipos de report'),
+        # }
