01e7010ec1b6eb35fea7852ec7bd1eae0a080946
Tiago Almeida <tyagow@hotmail.com.br>
2017-04-19 23:13:02 -0300

    Report Button e date input no keyboard mobile

--- src/reports/forms.py
+++ src/reports/forms.py
@@ -10,7 +10,12 @@ TIMESTAMP_FORMAT = "%d/%m/%Y %H:%M"
 
 
 class ReportForm(forms.ModelForm):
-    timestamp = forms.DateTimeField(label=_('Horário'), input_formats=[TIMESTAMP_FORMAT], initial=(timezone.now()-datetime.timedelta(hours=3)).strftime(TIMESTAMP_FORMAT))
+    timestamp = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'onfocus': 'blur()'}),
+        label=_('Horário'),
+        input_formats=[TIMESTAMP_FORMAT],
+        initial=(timezone.now()-datetime.timedelta(hours=3)).strftime(TIMESTAMP_FORMAT),
+    )
     type = forms.ChoiceField(label=_('Tipo de report'), choices=REPORT_TYPES, initial=1, widget=forms.HiddenInput())
 
     class Meta:
