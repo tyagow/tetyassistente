4d4c0e7571fc1073a17ab7d380a8eb3dba6e313c
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-08 18:07:35 -0300

    Delete report view added

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from src.core import views as core_views
-from src.reports.views import ReportListView, ReportCreateView, ReportDetailView
+from src.reports.views import ReportListView, ReportCreateView, ReportDetailView, ReportDeleteView
 
 urlpatterns = [
 
     url(r'^create$', login_required(ReportCreateView.as_view()), name='create'),
     url(r'^(?P<pk>\d+)$', login_required(ReportDetailView.as_view()), name='detail'),
+    url(r'^delete/(?P<pk>\d+)$', login_required(ReportDeleteView.as_view()), name='delete'),
     url(r'^$', login_required(ReportListView.as_view()), name='list'),
 ]
 
--- src/reports/views.py
+++ src/reports/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
@@ -10,16 +10,19 @@ class ReportListView(ListView):
     model = Report
 
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user)
+        return self.model.objects.filter(user=self.request.user).order_by('-timestamp')
 
 
 class ReportDetailView(DetailView):
     model = Report
 
 
+class ReportDeleteView(DeleteView):
+    model = Report
+
+
 class ReportCreateView(CreateView):
     model = Report
-    # fields = ['content', 'type', 'timestamp']
     form_class = ReportForm
 
     def form_valid(self, form):
